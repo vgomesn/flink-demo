92e2023d2045f4fbb4357c1d404b7613924ccf37
vgomesn <vgomesn@gmail.com>
2026-02-16 02:15:30 +0000

    new changes after ONA gitpod's automation
    Co-authored-by: Ona <no-reply@ona.com>

--- install_flink.sh
+++ install_flink.sh
@@ -7,15 +7,17 @@ if [ -d "flink-2.0.0" ]; then
 fi
 
 echo "Downloading Flink 2.0.0..."
-curl -sSL https://downloads.apache.org/flink/flink-2.0.0/flink-2.0.0-bin-scala_2.12.tgz -o flink-2.0.0.tgz
+curl -sSL https://archive.apache.org/dist/flink/flink-2.0.0/flink-2.0.0-bin-scala_2.12.tgz -o flink-2.0.0.tgz
 
 echo "Extracting..."
 tar -xzf flink-2.0.0.tgz
 rm -f flink-2.0.0.tgz
 
 # Bind UI for Gitpod & set a couple of sensible defaults
-sed -i 's@^#\?rest.bind-address:.*@rest.bind-address: 0.0.0.0@g' flink-2.0.0/conf/flink-conf.yaml || true
-grep -q '^parallelism.default' flink-2.0.0/conf/flink-conf.yaml || echo 'parallelism.default: 2' >> flink-2.0.0/conf/flink-conf.yaml
-grep -q '^taskmanager.numberOfTaskSlots' flink-2.0.0/conf/flink-conf.yaml || echo 'taskmanager.numberOfTaskSlots: 2' >> flink-2.0.0/conf/flink-conf.yaml
+CONF="flink-2.0.0/conf/flink-conf.yaml"
+sed -i 's@^#\?rest.bind-address:.*@rest.bind-address: 0.0.0.0@g' "$CONF" || true
+grep -q '^rest.bind-address' "$CONF" || echo 'rest.bind-address: 0.0.0.0' >> "$CONF"
+grep -q '^parallelism.default' "$CONF" || echo 'parallelism.default: 2' >> "$CONF"
+grep -q '^taskmanager.numberOfTaskSlots' "$CONF" || echo 'taskmanager.numberOfTaskSlots: 2' >> "$CONF"
 
 echo "Flink installed at: $(pwd)/flink-2.0.0"
