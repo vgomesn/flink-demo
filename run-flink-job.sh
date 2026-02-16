9cfe1354357a5462086418514692053b86b0bf02
vgomesn <vgomesn@gmail.com>
2026-02-16 01:48:23 +0000

    Multiple changes regarding Flink UI and PATH, etc.
    Co-authored-by: Ona <no-reply@ona.com>

--- run-flink-job.sh
+++ run-flink-job.sh
@@ -1,3 +1,13 @@
 mvn -q -DskipTests clean package
 netstat -an | grep 8081 | grep LISTEN
-flink run -c com.globomantics.UniqueAdClickAggregator target/flink-unique-clicks-1.0.0-shaded.jar out
+export PATH=$PATH:/workspaces/flink-demo/flink-2.0.0/bin
+#flink run -c com.globomantics.UniqueAdClickAggregator target/flink-unique-clicks-1.0.0-shaded.jar out
+flink run -c com.globomantics.UniqueAdClickAggregator target/flink-unique-clicks-1.0.0.jar out
+
+#ONA:
+# https://app.gitpod.io/details/019c6334-fa1a-7f14-893c-a759409d352d
+
+#Flink UI:
+# https://8081--019c6334-fa1a-7f14-893c-a759409d352d.us-east-1-01.gitpod.dev/#/overview
+
+
